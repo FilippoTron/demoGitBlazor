2b73c807584e53ce80bdf4e733e875669166d18a
Filippo <filippo.tron@aponemail.com>
2023-11-27 12:53:17 +0100

    Inserite entities

---
+++ DemoGitBlazor/Models/CircoloPadel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace Padel.Core.Entities;
+
+
+public class CircoloPadelGenerico<T,U>
+{
+    public string Nome { get; set; } = "";//con set inizializzare sempre
+    public Indirizzo? Indirizzo { get; set; }
+    public List<T>? Persone { get; set; }
+    public List<U>? AltrePersone { get; set; }
+
+    public CircoloPadelGenerico()
+    {
+            Persone = new List<T>();    
+            AltrePersone = new List<U>();
+    }
+
+    public void AggiungiPersona (T persona)
+    {
+        Persone?.Add(persona);
+    }
+
+    public void AggiungiAltraPersona(U persona)
+    {
+        AltrePersone?.Add(persona);
+    }
+}
+
+
+    
+       
+
+	
+
+
+public class CircoloPadel
+{
+    public string Nome { get; set; } = "";//con set inizializzare sempre
+    public Indirizzo? Indirizzo { get; set; }
+    public int Id { get; set; }
+
+
+    //public GiocatorePadel[] Giocatori { get; set; } = new GiocatorePadel[1000];
+    //public Istruttore[] Istruttori { get; set; } = new Istruttore[1000];
+    public List<GiocatorePadel>? Giocatori { get; set; } 
+    public List<Istruttore>? Istruttori { get; set; }    
+}
+
---
+++ DemoGitBlazor/Models/GiocatorePadel.cs
@@ -0,0 +1,45 @@
+﻿namespace Padel.Core.Entities;
+public class GiocatorePadel:Persona 
+{
+    #region
+    // private string Nome;
+    //public string Nome { get; set; }
+    //private string Cognome;
+    //public string Nome { get; init; } //init può essere solo inizializzato e non più modificato
+    //public string Cognome { get; init; }
+    //public string NumeroTelefono { get; set; } //set può essere sempre modificato
+
+    //public GiocatorePadel()
+    //{
+
+    //}
+    //public GiocatorePadel(string nome, string cognome)
+    //{
+    //    Nome = nome;
+    //    Cognome = cognome;
+    //}
+
+    //public GiocatorePadel(string nome, string cognome, string numeroTelefono)
+    //    : this(nome,cognome)
+    //{
+    //    NumeroTelefono = numeroTelefono;
+    //}
+    #endregion
+
+    public string NumeroTessera { get; set; }
+    public DateTime DataScadenzaIscrizione { get; set; }
+    public CircoloPadel Circolo { get; set; }
+    public bool TesseraValida { get; set; }
+
+    //public bool TesseraValida()
+    //{
+    //    return DataScadenzaIscrizione > DateTime.Now;
+    //}
+    //public int IdCircolo { get; set; }
+
+
+    public string DammiNomeCompleto(string separatore)
+    {
+        return $"{Nome} {separatore} {Cognome}";
+    }
+}
